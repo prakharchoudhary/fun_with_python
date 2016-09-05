9b358b8024bef0e60ec23cdccc11bf858bc06316
prakharchoudhary <prakhar13.cool@gmail.com>
2016-09-05 20:22:46 +0530

    user friendly

--- break_time.py
+++ break_time.py
@@ -1,9 +1,24 @@
+import os
 import time
 import webbrowser
 
-i = 0
-repeat = 3
-while(i < repeat):
-	time.sleep(1800)			#plays at an interval of 1800 secs or half an hour
+def clear():
+	os.system('cls' if os.name == 'nt' else 'clear')
+
+def timer(t):
+	#t = t * 60
+	time.sleep(t)			#plays at an interval of 1800 secs or half an hour
 	webbrowser.open("https://www.youtube.com/watch?v=Jm5DjptGtJo")
-	i = i + 1
+	c = raw_input("Do you wish to restart the timer?[y/n]: ")
+	if c=='y':
+		print "-"*40 + "\n"
+		t = int(raw_input("Set time(leave blank for 30 mins): "))
+		timer(t)
+	elif c == 'n':
+		print "*"*40
+		print "Awesome! You finished your work!"
+		return
+
+clear()
+T = int(raw_input('Set the time(in mins) for your study interval: '))
+timer(T)
