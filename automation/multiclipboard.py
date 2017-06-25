67d3cbac657cfd903f081928c652d2c4396a31e3
prakharchoudhary <prakhar2397@gmail.com>
2017-06-25 17:53:28 +0530

    add multiclipboard

---
+++ automation/multiclipboard.py
@@ -0,0 +1,22 @@
+#! usr/bin/python3
+# mcb.pyw - Saves and loads pieces of text to the clipboard.
+# Usage: py.exe mcb.pyw save <keyword> - Saves clipboard to keyword.
+# py.exe mcb.pyw <keyword> - Loads keyword to clipboard.
+# py.exe mcb.pyw list - Loads all keywords to clipboard.
+
+import shelve, pyperclip, sys
+
+mcbShelf = shelve.open('mcb')
+
+#TODO: Save clipboard content
+if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
+	mcbShelf[sys.argv[2]] = pyperclip.paste()
+
+elif len(sys.argv) == 2:
+#TODO: List keywords and load content
+	if mcbShelf[sys.argv[1]] == 'list':
+		pyperclip.copy(str(list(mcbShelf.keys())))
+	elif sys.argv[1] in mcbShelf:
+		pyperclip.copy(mcbShelf[sys.argv[1]])
+
+mcbShelf.close() 
