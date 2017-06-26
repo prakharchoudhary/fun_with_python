50850f22e09b27f4ede079f408c7ae7211112091
prakharchoudhary <prakhar2397@gmail.com>
2017-06-26 23:52:32 +0530

    made a few changes

--- automation/xkcddownloader.py
+++ automation/xkcddownloader.py
@@ -17,7 +17,7 @@ while not url.endswith('#'):
 	res = requests.get(url)
 	res.raise_for_status()
 
-	soup = bs4.BeautifulSoup(res.text)
+	soup = bs4.BeautifulSoup(res.text, 'lxml')
 	#TODO: Find url to next page
 
 	comicElem = soup.select('#comic img')
