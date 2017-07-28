980a1b52cf51665034867e4ea922c560a162e7f8
prakharchoudhary <prakhar2397@gmail.com>
2017-07-28 23:03:17 +0530

    add geocode and findRestaurant

---
+++ google_API/geocode.py
@@ -0,0 +1,14 @@
+import httplib2
+import json
+
+def getGeocodeLocation(inputString):
+	# Use Google Maps to convert a location into Latitute/Longitute coordinates
+	# FORMAT: https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=API_KEY
+	google_api_key = "AIzaSyCpYBOkN1EvC4i-hP24Me8HI6GADz6DgV8"
+	locationString = inputString.replace(" ", "+")
+	url = ('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'% (locationString, google_api_key))
+	h = httplib2.Http()
+	result = json.loads(h.request(url,'GET')[1])
+	latitude = result['results'][0]['geometry']['location']['lat']
+	longitude = result['results'][0]['geometry']['location']['lng']
+	return (latitude,longitude)
