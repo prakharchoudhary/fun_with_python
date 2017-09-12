ab57f9fd1482260c19a30972824e771d7736916e
prakharchoudhary <prakhar2397@gmail.com>
2017-09-12 19:10:41 +0530

    stopwatch using time module

---
+++ miscellaneous/stopwatch.py
@@ -0,0 +1,26 @@
+#! python3
+# A simple stopwatch program.
+
+import time
+
+# Display the program's instructions.
+print('Press ENTER to begin. Afterwards, press ENTER to "click" the stopwatch. Press CTRL-C to quit.')
+input()
+print('Started.')
+startTime = time.time()			# get the first lap's start time
+lastTime = startTime
+lapNum = 1
+
+# TODO: Start tracking the lap times.
+try:
+	while True:
+		input()
+		lapTime = round(time.time() - lastTime, 2)
+		totalTime = round(time.time() - startTime, 2)
+		print("Lap #%s: %s (%s)" % (lapNum, totalTime, lapTime), end='')
+		lapNum += 1
+		lastTime = time.time()  # reset the last lap time
+
+except KeyboardInterrupt:
+	# Handle the Ctrl-C exception to keep its error message from displaying.
+	print("\nDone") 
