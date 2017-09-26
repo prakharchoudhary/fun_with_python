2ea8ac128a618cc19e1f9f24aa7f45131d11a61d
prakharchoudhary <prakhar2397@gmail.com>
2017-09-27 01:16:07 +0530

    add anonymous and bidirectional pipes

---
+++ interprocess_comm/pipes/pipe-thread.py
@@ -0,0 +1,20 @@
+# anonymous pipes and threads, not processes;
+
+import os, time, threading
+
+def child(pipeout):
+	zzz = 0
+	while True:
+		time.sleep(zzz)								# make a parent wait
+		msg = ('Spam %03d' % zzz).encode()			# pipes are binary bytes
+		os.write(pipeout, msg)						# send to parent
+		zzz = (zzz+1) % 5							# goto 0 after 4
+
+def parent(pipein):
+	while True:
+		line = os.read(pipein, 32)					# blocks until data is sent
+		print('Parent %d got [%s] at %s' % (os.getpid(), line, time.time()))
+
+pipein, pipeout = os.pipe()
+threading.Thread(target=child, args=(pipeout,)).start()
+parent(pipein)
