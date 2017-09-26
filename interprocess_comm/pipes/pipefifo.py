fe8e3ee70dbe6f175cbdf19bc6e537accfd92987
prakharchoudhary <prakhar2397@gmail.com>
2017-09-27 01:45:32 +0530

    building named pipes

---
+++ interprocess_comm/pipes/pipefifo.py
@@ -0,0 +1,26 @@
+import os, time, sys
+
+fifoname = '/tmp/pipefifo'							# must open same name
+
+def child():
+	pipeout = os.open(fifoname, os.O_WRONLY)		# open fifo pipe file as fd
+	zzz = 0
+	while True:
+		time.sleep(zzz)
+		msg = ('Spam %03d\n' % zzz).encode()		# binary as opened here
+		os.write(pipeout, msg)
+		zzz = (zzz+1) % 5
+
+def parent():
+	pipein = open(fifoname, 'r')					# open fifo as text file object
+	while True:
+		line = pipein.readline()[:-1]				# block until data sent
+		print('Parent %d got "%s" at %s' % (os.getpid(), line, time.time()))
+
+if __name__ == '__main__':
+	if not os.path.exists(fifoname):
+		os.mkfifo(fifoname)							# create a named pipe file
+	if len(sys.argv) == 1:						
+		parent()									# run as parent if no args
+	else:											# else run as child parent
+		child()
