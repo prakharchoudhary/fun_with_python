b9b5ebbfa5c1b120dced6e68180aba6f6f0f7bfe
prakharchoudhary <prakhar2397@gmail.com>
2017-12-11 14:24:00 +0530

    add keyword support; fix threading conflict

--- automation/google_images.py
+++ automation/google_images.py
@@ -13,23 +13,30 @@ else:
 
 class GoogleScraper(object):
 
-    def __init__(self, search):
-        self.search = ('%20').join(search.split(' '))
+    def __init__(self, search, keywords=None):
+        self.search = ('%20').join(search)
+        if keywords:
+            self.keywords = ('+').join(keywords)
         self.headers = {}
         self.headers['User-Agent'] = \
             "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
-        self.url = 'https://www.google.com/search?q={}&espv=2&biw=1366&bih=667&site=webhp&source=lnms&tbm=isch&sa=X&ei=XosDVaCXD8TasATItgE&ved=0CAcQ_AUoAg'.format(
-            search)
+        if not keywords:
+            self.url = 'https://www.google.com/search?q={}&espv=2&biw=1366&bih=667&site=webhp&source=lnms&tbm=isch&sa=X&ei=XosDVaCXD8TasATItgE&ved=0CAcQ_AUoAg'.format(
+                self.search)
+        else:
+            self.url = 'https://www.google.com/search?q={},g_2={}&espv=2&biw=1366&bih=667&site=webhp&source=lnms&tbm=isch&sa=X&ei=XosDVaCXD8TasATItgE&ved=0CAcQ_AUoAg'.format(
+                self.search, self.keywords)
+        print(self.url)
         self.k = 0
         self.errorCount = 0
 
-    def get_soup(self, url, header):
+    def get_soup(self):
         return BeautifulSoup(
-            urlopen(Request(url, headers=self.headers)), 'html.parser')
+            urlopen(Request(self.url, headers=self.headers)), 'html.parser')
 
     def _get_all_images(self):
         AllImages = []  # contains the link for Large original images, type of  image
-        soup = self.get_soup(self.url, self.headers)
+        soup = self.get_soup()
         for url in soup.find_all("div", {"class": "rg_meta"}):
             link, Type = json.loads(
                 url.text)["ou"], json.loads(
@@ -42,11 +49,13 @@ class GoogleScraper(object):
             try:
                 req = Request(link, headers=self.headers)
                 img = urlopen(req, None, 15)
-                output_file = open(self.search + "/" +
-                                   str(self.k + 1) + ".jpg", 'wb')
-
-                data = img.read()
-                output_file.write(data)
+                try:
+                    output_file = open((' ').join(self.search.split('%20')) + "/" +
+                                       str(self.k + 1) + ".jpg", 'wb')
+                    data = img.read()
+                    output_file.write(data)
+                except Exception as e:
+                    print(e)
                 img.close()
 
                 print("completed ====> " + str(self.k + 1))
@@ -70,9 +79,12 @@ class GoogleScraper(object):
         downloadThreads = []                # a list of all the Thread objects
         for i in range(0, len(items), int(len(items) / 10)):
             downloadThread = threading.Thread(
-                target=self._write_images, args=(items, i, i + 9))
+                target=self._write_images, args=(items, i, i + 10))
             downloadThreads.append(downloadThread)
             downloadThread.start()
+
+        for downloadThread in downloadThreads:
+            downloadThread.join()
 
         print("\nImages Downloaded!")
         print("Error Count: %s" % (self.errorCount))
@@ -81,12 +93,13 @@ class GoogleScraper(object):
 def main():
     if len(sys.argv) > 1:
         if sys.argv[1] == '--search' or sys.argv[1] == '-S':
-            searchFor = GoogleScraper(sys.argv[2])
-            if not os.path.exists('./{}'.format(sys.argv[2])):
-                os.mkdir('./{}'.format(sys.argv[2]))
+            searchFor = GoogleScraper(sys.argv[2:])
+            dirname = (' ').join(sys.argv[2:])
+            if not os.path.exists('./{}'.format(dirname)):
+                os.mkdir('./{}'.format(dirname))
     else:
-        s = input("Enter the search keywords: ")
-        searchFor = GoogleScraper(s)
+        s = input("Enter the search term: ").split(' ')
+        searchFor = GoogleScraper(search=s)
         if not os.path.exists('./{}'.format(s)):
             os.mkdir('./{}'.format(s))
 
